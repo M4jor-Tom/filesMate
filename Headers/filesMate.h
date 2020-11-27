b31f591e6d9aa8af91752f30cf487a58d8d1b0df
Tom <tom-vautray@hotmail.fr>
2020-11-27 15:50:56 +0100

    Solved? write WIP read

--- Headers/filesMate.h
+++ Headers/filesMate.h
@@ -1,14 +1,35 @@
 #pragma once
 #include <list>
 #include <string>
-#include <cstdio>
-#include <cstdlib>
-#include <cstdbool>
+#include <fstream>
 
 using namespace std;
 
-//Reads a file and loads its content with given type
-template <typename T> list<T> readFile(string path);
+template <typename T> list<T> readFile(string path, T type)
+{
+	ifstream file(path);
+	list<T> content;
+	if (file.is_open())
+	{
+		file.read((char *)&content, sizeof(T));
 
-//OverWrites a file, creates it if it doesn't exist, and writes data
-template <typename T> bool writeFile(list<T> toWrite, const char *path);
+		file.close();
+	}
+
+	return content;
+}
+
+template <typename T> bool writeFile(list<T> toWrite, const char* path)
+{
+	ofstream file(path);
+
+	if (file.is_open())
+	{
+		file.write((const char *)&toWrite, sizeof(toWrite));
+
+		file.close();
+	}
+	else return false;
+
+	return true;
+}
